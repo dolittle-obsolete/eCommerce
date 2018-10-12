e7dda32725ad28bbb7ee4355d555733e2df064d3
Einar Ingebrigtsen <einar@dolittle.com>
2018-10-12 21:26:05 +0200

    Adding webpacking

--- run.cmd
+++ run.cmd
@@ -1,7 +1,9 @@
 @echo off
 cd Source\Shop\Web
 dotnet restore --ignore-failed-sources
+webpack
 start /b dotnet run --no-restore
 cd ..\..\Warehouse\Web
 dotnet restore --ignore-failed-sources
+webpack
 start /b dotnet run --no-restore
